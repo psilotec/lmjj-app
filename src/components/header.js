d2898d33309321335d1ae912b085405757070010
psilotec <scottadevito@gmail.com>
2017-03-06 00:50:29 -0500

    Wire up Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Menu, Dropdown } from 'semantic-ui-react';
 
 const Header = () => {
     return(
-        <div>Header</div>
+        <div>
+            <Menu pointing secondary>
+                <Link to="/" className="item">
+                    JJ Guide
+                </Link>
+                <Link to="beltlist" className="item">
+                    Belt List
+                </Link>
+                <Link to="seminars" className="item">
+                    Seminars
+                </Link>
+                <Link to="planner" className="item">
+                    Lesson Planner
+                </Link>
+                <a className="item">
+                    Login
+                </a>
+                <Menu.Menu position="right">
+                    <Dropdown icon="ellipsis vertical large" className="item">
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Edit Technique</Dropdown.Item>
+                            <Dropdown.Item>Admin Console</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Menu>
+            </Menu>
+        </div>
     );
 }
 
