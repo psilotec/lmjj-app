1d947c7fe3735f0a6063405ce12b31526d70910c
psilotec <scottadevito@gmail.com>
2017-03-08 17:39:48 -0500

    Set up middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './App';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 ReactDOM.render(
-  <Provider store={createStore(() => {})}>
+  <Provider store={createStoreWithMiddleware((reducers) => {})}>
     <App />
   </Provider>
   , document.getElementById('root'));
