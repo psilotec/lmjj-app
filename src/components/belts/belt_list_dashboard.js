4e850d422944e1707a1d8ca0842e409d60f9c8cf
psilotec <scottadevito@gmail.com>
2017-03-08 16:40:53 -0500

    Start building BeltListDashboard

--- src/components/belts/belt_list_dashboard.js
+++ src/components/belts/belt_list_dashboard.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BeltListDashboard = () => {
-    return(
-        <div>BeltListDashboard</div>
-    );
+import BeltList from './belt_list';
+
+class BeltListDashboard extends Component {
+    renderBeltList() {
+        return "BeltList";
+    }
+
+    render() {
+        return(
+            <div className="beltlist-flexwrap">
+                <BeltList renderBeltList={this.renderBeltList} />
+                TechniqueList
+            </div>
+        );
+    }
 }
 
 export default BeltListDashboard;
