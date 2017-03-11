7e44f9240c5542c32ed9af13969fc7fdd2e0c008
psilotec <scottadevito@gmail.com>
2017-03-10 21:14:41 -0500

    Return belts obj from Firebase

--- src/actions/action-belts.js
+++ src/actions/action-belts.js
@@ -7,18 +7,9 @@ const Belts = database.ref().child('belts');
 const fetchBelts = () => {
     return (dispatch) => {
         Belts.on('value', snapshot => {
-            const obj = snapshot.val();
-            let beltArr = [];
-
-            for (let key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                    beltArr.push(obj[key]);
-                }
-            }
-
             dispatch({
                 type: FETCH_BELTS,
-                payload: beltArr,
+                payload: snapshot.val();,
             })
         })
     };
