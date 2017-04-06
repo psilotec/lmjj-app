7cb3a65b58d176369045e2054d27d5cad3c62f00
psilotec <scottadevito@gmail.com>
2017-04-06 14:01:01 -0400

    Adjust containers

--- src/components/techniques/technique_card.js
+++ src/components/techniques/technique_card.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react'
+import { Card, Image, Container, Divider } from 'semantic-ui-react'
 
 const TechniqueCard = ({ selectedTechnique }) => {
     const src = 'https://s3.amazonaws.com/miscbucket444/lmjjshot.png';
     return (
-        <div>
-            <Card fluid color={selectedTechnique.techDisplayColor}>
+        <Container>
+            <Card fluid centered color={selectedTechnique.techDisplayColor}>
                 <Card.Content>
                     <Card.Header>{selectedTechnique.techName}</Card.Header>
                 </Card.Content>
-                <Card.Content>
-                    <Image src={src} />
-                </Card.Content>
+                <Divider />
+                <Image src={src} centered />
                 <Card.Content>
                     <Card.Description>
                         {selectedTechnique.techDesc}
                     </Card.Description>
                 </Card.Content>
             </Card>
-        </div>
+        </Container>
     );
 };
 
