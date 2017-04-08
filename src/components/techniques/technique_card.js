19691e00aff329a559d7a585ec58132c61f7490a
Scott DeVito <scottadevito@gmail.com>
2017-04-07 22:04:23 -0400

    Make grid layout more explicit

--- src/components/techniques/technique_card.js
+++ src/components/techniques/technique_card.js
@@ -4,22 +4,24 @@ import { Card, Image, Grid, Header } from 'semantic-ui-react'
 const TechniqueCard = ({ selectedTechnique }) => {
     const src = 'https://s3.amazonaws.com/miscbucket444/lmjjshot.png';
     return (
-        <Grid centered columns={1}>
-            <Grid.Column>
-                <Card className='techniquecard' centered color={selectedTechnique.techDisplayColor}>
-                    <Card.Content>
-                        <Header size='medium'>{selectedTechnique.techName}</Header>
-                    </Card.Content>
-                    <Card.Content>
-                        <Image className='technique-image' src={src} centered />
-                    </Card.Content>
-                    <Card.Content>
-                        <Card.Description>
-                            {selectedTechnique.techDesc}
-                        </Card.Description>
-                    </Card.Content>
-                </Card>
-            </Grid.Column>
+        <Grid>
+            <Grid.Row centered columns={1}>
+                <Grid.Column>
+                    <Card className='techniquecard' centered color={selectedTechnique.techDisplayColor}>
+                        <Card.Content>
+                            <Header size='medium'>{selectedTechnique.techName}</Header>
+                        </Card.Content>
+                        <Card.Content>
+                            <Image className='technique-image' src={src} centered />
+                        </Card.Content>
+                        <Card.Content>
+                            <Card.Description>
+                                {selectedTechnique.techDesc}
+                            </Card.Description>
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
+            </Grid.Row>
 
             <Grid.Row centered columns={3} only='tablet computer'>
                 <Grid.Column>
